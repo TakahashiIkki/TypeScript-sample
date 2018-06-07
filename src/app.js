fbc8e4be0f50eb54ee6316fea2967c838bebb046
TakahashiIkki <new_ittyandesu@outlook.jp>
2018-06-08 00:25:20 +0900

    httpモジュールを使ってhttpサーバーを立てた

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const http = require("http");
 // classを指定してクラス化
 class MyClass {
     // コンストラクター
     constructor() {
         this.message = 'TypeScript';
-        console.log('Hello! Node.js & ' + this.message);
+        // httpサーバーの設定
+        const server = http.createServer((request, response) => this.requestHandler(request, response));
+        // サーバーを起動してリクエスト待機状態とする.
+        server.listen('5001');
+    }
+    requestHandler(request, response) {
+        response.end('Hello World & ' + this.message + ' for HTTP');
     }
 }
 // Defaultクラスのインスタンスを作る
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,22 @@
+import * as http from 'http';
+
 // classを指定してクラス化
 class MyClass {
     message: String = 'TypeScript';
 
     // コンストラクター
     constructor() {
-        console.log('Hello! Node.js & ' + this.message);
+        // httpサーバーの設定
+        const server: http.Server = http.createServer (
+            (request: http.IncomingMessage, response: http.ServerResponse) => 
+                this.requestHandler(request, response));
+        
+        // サーバーを起動してリクエスト待機状態とする.
+        server.listen('5001');
+    }
+
+    private requestHandler(request: http.IncomingMessage, response: http.ServerResponse) : void {
+        response.end('Hello World & ' + this.message + ' for HTTP');
     }
 }
    
